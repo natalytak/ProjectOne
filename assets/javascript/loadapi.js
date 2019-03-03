67d8251760874ba39308badc22b77d595c7b275b
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-03-03 18:24:51 -0500

    added news.html events.html and re-styled pages

--- assets/javascript/loadapi.js
+++ assets/javascript/loadapi.js
@@ -1,7 +1,9 @@
+// call the 'city' atteibute from LocalStorage and create the cityInput attribute
 var cityInput = localStorage.getItem('city');
 
 console.log(cityInput);
 
+// create weather api call query variable which includes the dynamic cityInput 
 var queryURL =
   "http://api.openweathermap.org/data/2.5/weather?q=" +
   cityInput +
@@ -9,6 +11,7 @@ var queryURL =
 
 console.log(queryURL);
 
+// weather api ajax call request and response logic
 $.ajax({
   url: queryURL,
   method: "GET"
@@ -17,11 +20,11 @@ $.ajax({
 
   var results = response;
 
-  // adding temperature to currentWeather div element on UI
+  // add temperature to currentWeather div element on UI, rounding temp using Math.round function because temp value come back with two decimal places by default
   var roundedTemp = Math.round(results.main.temp);
-  var currentTemp = $("<div>").text(roundedTemp + " Degrees F");
+  var currentTemp = $("<div>").text("Weather: " + roundedTemp + " Degrees F");
 
-
-  $("#currentCityID").prepend("Your're looking at " + cityInput);
+  // add temperature and city values to menu UI
+  $("#currentCityID").prepend(cityInput);
   $("#currentWeather").prepend(currentTemp);
 });
