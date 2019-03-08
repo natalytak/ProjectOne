e24428510f8b39956eb412199f090bc684b2b26c
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-03-07 23:02:46 -0500

    0f558b66adcbf68bdb05a6e088c378782a9a8064

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,51 @@
 $(document).ready(function(){
 
-    $(".fade-images").hide();
-    $(window).scroll(function() {
-        $(".fade-images").css( "display", "inline" ).fadeIn("slow");
-      });
+  // transition style functions & variables
+  var $overviewImage = $(".overview-image");
+    $overviewImage.waypoint(function() {
+    $overviewImage.addClass("overview-image-waypoint");
+    }, { offset: "50%"});
 
 
+    var $attractionsImage = $(".attractions-image");
+    $attractionsImage.waypoint(function() {
+    $attractionsImage.addClass("attractions-image-waypoint");
+    }, { offset: "50%"});
+
+    var $newsImage = $(".news-image");
+    $newsImage.waypoint(function() {
+    $newsImage.addClass("news-image-waypoint");
+    }, { offset: "50%"});
+
+    var $aboutUsStyle = $(".about-us-fade")
+    $aboutUsStyle.waypoint(function() {
+    $aboutUsStyle.addClass("about-us-fade-waypoint");
+    }, { offset: "70%"});
+
+    var $theTeamStyle = $(".the-team-title-style")
+    $theTeamStyle.waypoint(function() {
+    $theTeamStyle.addClass("the-team-title-style-waypoint");
+    }, { offset: "70%"});
+
+    var $theTeamStyleGroupOne = $(".the-team-style-group-one")
+    $theTeamStyleGroupOne.waypoint(function() {
+    $theTeamStyleGroupOne.addClass("the-team-style-group-one-waypoint");
+    }, { offset: "70%"});
+
+    var $theTeamStyleGroupTwo = $(".the-team-style-group-two")
+    $theTeamStyleGroupTwo.waypoint(function() {
+    $theTeamStyleGroupTwo.addClass("the-team-style-group-two-waypoint");
+    }, { offset: "70%"});
+
+    var $theTeamStyleGroupThree = $(".the-team-style-group-three")
+    $theTeamStyleGroupThree.waypoint(function() {
+    $theTeamStyleGroupThree.addClass("the-team-style-group-three-waypoint");
+    }, { offset: "70%"});
+    
+    // end transition
+
+
+    // dropdown button array
     var stateList= [{name: "Alabama", shortName: "AL"}, {name: "Arkansas"}, {name: "Arizona"}, {name: "California"}, {name:"Colorado"}, {name: "Connecticut"},{name:"Delaware"}, {name: "Florida"}, {name: "Georgia"}, {name: "Hawaii"}, {name: "Idaho"}, {name: "Illinois"}, {name: "Indiana"}, {name: "Iowa"}, {name:"Kansas"}, {name: "Kentucky"}, {name: "Lousiana"}, {name: "Maine"}, {name: "Maryland"}, {name: "Massachusetts"}, {name: "Michigan"}, {name: "Minnesota"}, {name: "Missippi"}, {name: "Missouri"}, {name: "Montana"}, {name: "Nebraska"}, {name: "Nevada"}, {name: "New Hampshire"}, {name: "New Jersey"}, {name: "New Mexico"}, {name: "New York"}, {name: "North Carolina"}, {name: "North Dakota"}, {name: "Ohio"}, {name: "Oklahoma"}, {name: "Oregon"}, {name: "Pennsylvania"}, {name: "Rhode Island"}, {name: "South Carolina"}, {name: "South Dakota"}, {name: "Tennessee"}, {name: "Texas"}, {name: "Utah"}, {name: "Vermont"}, {name: "Virginia"}, {name: "Washington"}, {name: "West Virginia"}, {name: "Wisconsin"}, {name: "Wyoming"}];
     
     for (var i = 0; i < stateList.length; i++) {
@@ -15,9 +55,9 @@ $(document).ready(function(){
               $("#dropdownMenuButton").text(selectedState);
         });
     };
+    
 
-
-
+    // form validation
     $('.form-control').on('keypress', function(e) {
       var regex = new RegExp("^[a-zA-Z ]*$");
       var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
@@ -29,7 +69,7 @@ $(document).ready(function(){
      });
 
 
-
+    //  user city input value
      $("#citySearchBtn").on("click", function() {
     
       event.preventDefault();
@@ -47,7 +87,7 @@ $(document).ready(function(){
 
 
 
-
+      // feeling spontaneous button
     var randomCityList = ["Austin", "New York", "Las Vegas", "Philadelphia", "Chicago", "Nashville", "Miami", "Denver", "Anchorage", "Honolulu"];
 
     $("#randomCityButton").on("click", function() {
