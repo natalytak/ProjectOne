0562de09142871b42bffee1c6bf61ee2ed22735f
Tasha <natalytak@yahoo.com>
2019-03-08 18:28:50 -0500

    added broken image backup

--- assets/javascript/loadapi.js
+++ assets/javascript/loadapi.js
@@ -48,7 +48,7 @@ var pixabayUrl = "https://pixabay.com/api/?key=11801190-0ca7f6e1ad42b7ceca325ed2
 
 console.log(cityInput);
 
-    var attractionsUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tourist-attractions&location=" + cityInput + "&sort_by=rating&limit=5";
+    var attractionsUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tourist-attractions&location=" + cityInput + "&sort_by=rating&limit=3";
          
     callYelp(attractionsUrl);
 
@@ -75,7 +75,15 @@ console.log(cityInput);
                 // Append our result into our page
                 var businessesDiv = $('<div class="attractions">');
                 businessesDiv.append('<div id="' + id + '" style="margin-top:5px;margin-bottom:5px;">');
-                businessesDiv.append('<img id="attractionImage" src="' + image + '" style="width:100%;height:auto;"><br>');
+                var attractionDiv = $('<img class="attractionImage" id="' + id + '" src="' + image + '"style="width:100%;height:auto"><br>');
+                // to put as back-up image in case the one from Yelp API does not load
+                $(".attractionImage").on("error", function() {
+                console.log("image not loaded");
+                $(this).unbind("error").attr("src", "https://cdn.pixabay.com/photo/2018/05/10/11/34/concert-3387324__480.jpg");
+                console.log("changed attr");
+                });
+
+                businessesDiv.append(attractionDiv);
                 businessesDiv.append('<b id="name">' + name + '</b><br>');
                 businessesDiv.append('<b>Rating: </b>' + rating + '<br>');
                 businessesDiv.append('<b>Address: </b>' + address + '<br>');
@@ -117,18 +125,26 @@ success: function(data){
       var eventsDiv = $('<div class="events">');
       
       eventsDiv.append('<div id="' + id + '" style="margin-top:5px;margin-bottom:5px;">');
-      eventsDiv.append('<img id="#eventImage" src="' + image + '" style="width:100%;height:250px;float:left"><br>');
+      var imageDiv = $('<img class="eventImage" id="' + id + '" src="' + image + '"style="width:100%;height:250px;float:left"><br>');
+      // to put as back-up image in case the one from Yelp API does not load
+      $(".eventImage").on("error", function() {
+        console.log("image not loaded");
+        $(this).unbind("error").attr("src", "https://cdn.pixabay.com/photo/2018/05/10/11/34/concert-3387324__480.jpg");
+        console.log("changed attr");
+      });
+
+      eventsDiv.append(imageDiv);
       eventsDiv.append('<b id="name">' + name + '</b><br>');
       eventsDiv.append(description + '<br>');
       eventsDiv.append('<b>Attending: </b>' + attending + '<br>');
       eventsDiv.append('<b>Address: </b>' + address + '<br>');
       eventsDiv.append('<b>Date: </b>' + timeStart + '<br>')
       eventsDiv.append('<button class="btn btn-secondary" id="details"><a target="_blank" href="' + eventURL + '">Details</a></button><br>');
-      
       $("#topEvents").prepend(eventsDiv);
+    
     });
   }
-});      
+});     
 
 ///////Natasha's overview API call/////
 
