b4dc038a1fbf961521dd07844f85aaaa193dad5c
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-03-08 20:40:38 -0500

    updates Ondell news changes

--- assets/javascript/api-news.js
+++ assets/javascript/api-news.js
@@ -1,185 +1,184 @@
 
-          
-          // call the 'city' atteibute from LocalStorage and create the cityInput attribute
-                var cityInput = localStorage.getItem('city');
-            
-          
-              function buildQueryURL() {
 
-                  method: "GET"
-                var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" 
-                var queryParams = { "api-key": "gEHdwjN6FzvV3qyy6BpAlyGyU4m9NGGP" };
-          
-                queryParams.q = cityInput
+// call the 'city' from LocalStorage and create the cityInput attribute
+var cityInput = localStorage.getItem('city');
+
+
+
+
+function buildQueryURL() {
+
+  method: "GET"
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+  var queryParams = { "api-key": "gEHdwjN6FzvV3qyy6BpAlyGyU4m9NGGP" };
+  queryParams.q = cityInput
+  queryParams.fq = ("news_desk:travel");
+  console.log(queryURL + $.param(queryParams));
+  return queryURL + $.param(queryParams);
+
+
+
+}
+
+      $("#cityInput1").append("NYT Travel Articles |" + " " + cityInput);
+
+
+  function updatePage(NYTData) {
+  var numArticles = 20;
+  console.log(numArticles);
+  console.log(NYTData);
+  for (var i = 0; i < numArticles; i++) {
+  var article = NYTData.response.docs[i];
+
+
+    var $articleList = $("<div class='card-header' style ='margin:0 auto;'>");
+
+    $(document).ready(function(){
+      $(".card-header").hover(function(){
+        $(this).css("background-color", "rgb(255,234,203)");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+    });
+
+
+  // URL to DIV
+  var articleimageLink = ("href", article.web_url);
+  console.log(articleimageLink);
+
+    
+      $(".card-header").click(function() {
+        window.location = articleimageLink;
+    });
+ 
     
 
-                queryParams.fq = ("news_desk:travel");
-                console.log(queryURL + $.param(queryParams));
-
-                return queryURL + $.param(queryParams);
-
-                
-                
-              }
-           //https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=gEHdwjN6FzvV3qyy6BpAlyGyU4m9NGGP&q=denver&fq=news_desk%3Atravel
-          
-              function updatePage(NYTData) {
-
-                var numArticles = 20;
-                console.log(numArticles);
-                console.log(NYTData);
-                for (var i = 0; i < numArticles; i++) {
-                  var article = NYTData.response.docs[i];
-          
-      
-                  // Variable to Contain the articles
-                  var $articleList = $("<div class='card-header'>");
-
-                  // Add the newly created element 
-                  $("#display-articles").append($articleList);
-
-          
-                  // If the article has a headline, log and append to $articleList
-                  var headline = article.headline;
-                  
-                  // Main div container
-                  var $articleListItem = $("<div style='color:white; background-color:rgba(52,58,64); padding:35px 20px;  width:500px; height:650px; margin: auto; text-align: center;'></div>");
-                
-                   // Image
-                  var articleimageLink =("href", article.web_url);
-                  console.log(articleimageLink);
-                 // $("#divLink").append(articleimageLink);
-                 //$("#divImage").click(function(){
-                  //window.location.href = this.getElementsByClassName(articleimageLink)[0].href;
-                  //});
-          
-          
-          
-                  var multimedia = ("http://nyt.com/" + article.multimedia[i].url);
-                  console.log(multimedia);
-          
-                  var image = $("<img>");
-                  image.attr('height', 300,);
-                  image.attr("src", multimedia);
-                  console.log(image);
-                    
-
-                  // Appending the paragraph and image tag to the animalDiv
-                  if (multimedia) {
-                    $articleListItem.append(image);
-                  }
+    // Add the newly created element 
+    $("#display-articles").append($articleList);
 
 
-                  if (headline && headline.main) {
-                    console.log(headline.main);
-                    $articleListItem.append(
-                      "<h4> " +"<div style='padding-top:15px;' 'padding-bottom:5px;'>"  + 
-                      headline.main +
-                      "</h4>" + 
-                      "</div>"
-                    );
-                    ;
-                  }
+ 
+    // Main div container
+    var $articleListItem = $("<div style='color:white; background-color:rgba(38,69,110); padding:35px;  width:375px; height:625px; margin:0 auto;'></div>");
 
 
-                
-                  // If the article has a byline, log and append to $articleList
-                  var byline = article.byline;
-          
-                  if (byline && byline.original) {
-                    console.log(byline.original);
-                    $articleListItem.append("<h6>" + byline.original + "</h6>");
-                  }
-          
-          
-                  // Log published date, and append to document if exists
-                  var pubDate = article.pub_date;
-                  console.log(pubDate);
-          
-                  //Using momen.js to convert date format
-                  var randomFormat = "YYYY-MM-DD";
-                  var convertedDate = moment(pubDate, randomFormat);
-                  var date = convertedDate.format("MMMM Do, YYYY");
-          
-          
-                  if (pubDate) {
-                    $articleListItem.append("<h6>" + date + "</h6>");
-                  }
-          
-          
-                  // Log published date, and append to document if exists
-          
-                  var snippet = article.snippet;
-                  console.log(snippet);
-                  if (snippet) {
-                    $articleListItem.append("<p>" + snippet + "<p>");
-                  }
-          
-          
+  
 
-          
-                  var articleButton = $("<button>");
-          
-          
-                  articleButton.addClass("btn btn-warning");
-                  articleButton.addClass("fa fa-newspaper-o");
-                  articleButton.text(" " + "Read Article");
-                  var articleLink = $("<a>");
-                  articleLink.attr("href", article.web_url);
-                  articleLink.append(articleButton);
-                  // articleButton.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
-                  console.log(article.web_url);
-                  $articleListItem.append(articleLink);
-                  // $(articleLink).clone().appendTo("#user-button");
-                  $articleList.append($articleListItem);
-          
-                  //var articleGrid = $('<div>');
-                  //articleGrid.addClass("col-sm-4"[i])
-                  //articleGrid.append($articleList);
-          
-                }
-          
-                  // Function to load more articles
-          
-          
-                var load = document.createElement("button");
-                load.setAttribute('id', "divLoadButton");
-                load.innerHTML = "Load More";
-                $("#divLoadButton").empty();
-                $("#divLoadButton").append(load);
-          
-                $("#divLoadButton").on("click", function () {
-          
-                  numArticles = numArticles + 3;
-                  console.log(numArticles);
-                  //i=i+10;
-                  console.log(i);
-   
 
-          
-          })
-          }
-          
+    // Image
+    var multimedia = ("http://nyt.com/" + article.multimedia[i].url);
+    console.log(multimedia);
+    var image = $("<img>");
+    image.attr('height', 225);
+    image.attr("src", multimedia);
+    console.log(image);
+
+
+    if (multimedia) {
+      $articleListItem.append(image);
+    }
+
+    if (multimedia) {
+      $articleListItem.append("<br><br><br><br><br><br><br>");
+    }
+
+   // Headline
+   var headline = article.headline;
+
+    if (headline && headline.main) {
+      console.log(headline.main);
+      //headline.wrap('<a href=' + multimedia + '/>');
+      $articleListItem.append( "<br><br>" +
+        "<h5 style='padding-top:15px;' 'padding-bottom:5px;' >" +
+        headline.main +
+        "</h5>" 
+      );
+      ;
+    }
+
+    
+
+
+    // Byline
+    var byline = article.byline;
+
+    if (byline && byline.original) {
+      console.log(byline.original);
+      $articleListItem.append("<h9>" + byline.original + "</h9>" + "<br>");
+    }
+
+
+    // Published date
+    var pubDate = article.pub_date;
+    console.log(pubDate);
+
+    //Moment.js to convert date format
+    var randomFormat = "YYYY-MM-DD";
+    var convertedDate = moment(pubDate, randomFormat);
+    var date = convertedDate.format("MMMM Do, YYYY");
+
+
+    if (pubDate) {
+      $articleListItem.append("<h9>" + date + "</h9>" + "<br>");
+    }
+
+
+    // Log published date, and append to document if exists
+    var snippet = article.snippet;
+    console.log(snippet);
+    if (snippet) {
+      $articleListItem.append("<p>" + snippet + "<p>");
+    }
+
+
+
+
+    var articleButton = $("<button>");
+    articleButton.addClass("btn btn-warning");
+    articleButton.addClass("fa fa-newspaper-o");
+    articleButton.text(" " + "Read Article");
+    var articleLink = $("<a>");
+    articleLink.attr("href", article.web_url);
+    articleLink.append(articleButton);
+    // articleButton.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+    console.log(article.web_url);
+    $articleListItem.append(articleLink);
+    // $(articleLink).clone().appendTo("#user-button");
+    $articleList.append($articleListItem);
+
+  }
+}
+
+
+
+
+              var queryURL = buildQueryURL();
+              $.ajax({
+                url: queryURL,
+                method: "GET"
+              }).then(updatePage);
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
               
 
-          
-              // Function to empty out the articles
-              function clear() {
-                $("#display-articles").empty();
-              }
-          
-          
-             var queryURL = buildQueryURL();
-                $.ajax({
-                  url: queryURL,
-                  method: "GET"
-                }).then(updatePage);
-              
-          
+    
 
-          
-          
-          
-          
-          
-           
+
+
+
